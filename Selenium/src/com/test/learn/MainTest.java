3533d2acf27d5738ad4834d6987aae6fa6135bf5
ASC <asc@github.com>
2018-10-02 01:56:25 +0530

    Added Main Class

---
+++ Selenium/src/com/test/learn/MainTest.java
@@ -0,0 +1,14 @@
+package com.test.learn;
+
+public class MainTest {
+
+	public MainTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
