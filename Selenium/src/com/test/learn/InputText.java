ad843f73addf5d38e1f44d96084b98d5e97cc59b
ASC <asc@github.com>
2018-10-21 19:39:29 +0530

    Updated to Maven Project

--- Selenium/src/com/test/learn/InputText.java
+++ Selenium/src/com/test/learn/InputText.java
@@ -1,16 +1,14 @@
 package com.test.learn;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class InputText {
 
 	public InputText() {
 		System.out.println("Constructor Defined");
 	}
-	public void InputTextBox(){
-		System.setProperty("webdriver.chrome.driver", "./lib/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+	public void InputTextBox(WebDriver driver){
+		
 		driver.get("https://www.google.co.in");
 		
 	}
--- Selenium/src/com/test/learn/MainTest.java
+++ Selenium/src/com/test/learn/MainTest.java
@@ -1,17 +1,17 @@
 package com.test.learn;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class MainTest {
 
-	public MainTest() {
-		String text = "Welcome";
-		System.out.println(text);
-	}
-
 	public static void main(String[] args) {
-		
-		MainTest mt = new MainTest();
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Abhishek\\Documents\\GitHub\\Selenium\\Selenium\\lib\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		// MainTest mt = new MainTest();
 		InputText it = new InputText();
-		it.InputTextBox();
+		it.InputTextBox(driver);
 		System.out.println("Hello World");
 	}
 
