004ec617647e0a47bd60ab2c3a824d26d7aeeacc
ASC <asc@github.com>
2018-10-21 22:25:58 +0530

    Updated to maven

--- Selenium/src/com/test/learn/MainTest.java
+++ Selenium/src/com/test/learn/MainTest.java
@@ -5,14 +5,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MainTest {
 
+	static WebDriver driver;
+
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\Abhishek\\Documents\\GitHub\\Selenium\\Selenium\\lib\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		// MainTest mt = new MainTest();
-		InputText it = new InputText();
-		it.InputTextBox(driver);
+		driver = new ChromeDriver();
+		MainTest mt = new MainTest();
+		mt.launchGoogle(driver);
+	}
+
+	public void launchGoogle(WebDriver driver) {
+		driver.get("www.google.com");
+		// InputText it = new InputText();
+		// it.InputTextBox(driver);
 		System.out.println("Hello World");
+		driver.close();
 	}
 
 }
